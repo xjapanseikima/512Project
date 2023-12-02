8d0b105da27b29ae2e3aada5c5483035befafcb1
xjapanseikima <magican213@gmail.com>
2023-12-02 16:48:17 -0500

    update the conclusion and description

--- 512project/ui.R
+++ 512project/ui.R
@@ -15,17 +15,25 @@ library(plotly)
 library(pheatmap)
 library(corrplot)
 library(reshape2)
-
-
 ui <- fluidPage(
   titlePanel("512 Project"),
+  titlePanel("Explore Factors that May Affect Sleep Quality"),
+  HTML("<br>"),
+  HTML("<p style='font-size: 16px;'>by Chun-Yu Tseng, Hang Lu, Qiaoyi Wu</p>"),
+  HTML("<br>"),
+  HTML("<p style='font-size: 16px;'>Description</p>"),
+  HTML("<br>"),
+  HTML("<p style='font-size: 16px;'>This data allows exploration and analysis of relationships between various health and lifestyle factors, potentially enabling insights into how sleep, physical activity, stress, and other factors relate to overall health and well-being. Analyzing this dataset could help identify correlations or patterns influencing individuals' health conditions and sleep-related issues.
+Overall, we have a several conclusion of what we found in these datasets</p>"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins","Number of bins:", min = 1, max = 60, value = 10),
+      p("These selction (xyz) will affect plot 2,3,4."),
       selectInput('xcol','X Variable', names(data)),
       selectInput('ycol','Y Variable', names(data)),
       selectInput('zcol','Z Variable (Only used for 3d plot)', names(data)),
       selected = names(data)[[2]],
+      p("Below selction will affect only in confusion matrix."),
       checkboxGroupInput("choices",
                          label = "Which category would you like to see in the Correlation plot?
                          Some variable have not categrrized yet..",
@@ -33,8 +41,8 @@ ui <- fluidPage(
                          selected = unique(data)),
       textOutput("txt"),
       p("Choose two variable you want to plot in the ploty and do analysis."),
-      p("Thinking...", style = "font-family: 'times'; font-si16pt"),
-      # Adding from here in the SideBar panel
+        menuItem("Description Table", tabName = "Conlusion Table"),
+        #menuItem("Conclusion 1", tabName = "Conclusion"),
     ),
     mainPanel(
       DT::dataTableOutput("table"),
@@ -43,7 +51,19 @@ ui <- fluidPage(
       plotOutput("distPlot"),
       plotOutput("x2"),
       plotlyOutput("threeDPlot"),
-      plotOutput("corrPlot")
+      plotOutput("corrPlot"),
+      tabItem(
+        tabName = "Conlusion Table",
+        HTML("<p style='font-size: 16px;'> Conlusion 1:
+        In this profound exploration of 400 individuals' lives, 
+       the Sleep Health and Lifestyle Dataset unveils the intricate dance between work, health, and sleep quality. 
+       Immerse yourself in a wealth of data covering sleep duration, quality, and lifestyle factors such as physical 
+       activity levels and stress ratings. With a keen eye on cardiovascular health, this dataset delves into blood 
+       pressure and heart rate metrics. The presence or absence of sleep disorders, including Insomnia and Sleep Apnea, 
+       adds a critical layer to understanding the holistic well-being of each participant. Join us in deciphering the 
+       profound connections within work, health, and the quality of sleep.</p>"),
+        DTOutput("table2")
+      ),
       # Adding from here in the Main Bar panel
     )
   )
